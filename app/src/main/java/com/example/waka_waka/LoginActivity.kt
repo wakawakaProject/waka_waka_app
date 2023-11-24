d25cd5f8c9080b575aa7de05f5ae640194a29724
jin064 <jyeong906@gmail.com>
2023-11-24 15:21:03 +0900

    Fix : LoginActivity fixed into binding

--- app/src/main/java/com/example/waka_waka/LoginActivity.kt
+++ app/src/main/java/com/example/waka_waka/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.example.waka_waka
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.waka_waka.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
 
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(binding.root)
+
     }
 }
