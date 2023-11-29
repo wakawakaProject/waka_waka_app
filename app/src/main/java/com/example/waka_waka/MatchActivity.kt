2c77eccdeed5d02064aa3f16fa6aa169b421c843
Hyowon Kang <hyowon3434@naver.com>
2023-11-30 01:09:21 +0900

    Feat: created matchacitivity

--- app/src/main/java/com/example/waka_waka/MatchActivity.kt
+++ app/src/main/java/com/example/waka_waka/MatchActivity.kt
@@ -2,12 +2,17 @@ package com.example.waka_waka
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import com.example.waka_waka.databinding.ActivityMatchCreateBinding
 
 class MatchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+
+        val binding = ActivityMatchCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
