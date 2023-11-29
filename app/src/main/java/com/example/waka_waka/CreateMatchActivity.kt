7a133e9baabc38b9ea45d67cf8e53e57e7d7c727
Hyowon Kang <hyowon3434@naver.com>
2023-11-30 01:35:38 +0900

    Feat:  spinner에 드랍다운 리스트 추가

---
+++ app/src/main/java/com/example/waka_waka/CreateMatchActivity.kt
@@ -0,0 +1,28 @@
+package com.example.waka_waka
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.waka_waka.databinding.ActivityMatchCreateBinding
+
+class CreateMatchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMatchCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // spinner에 리스트 추가
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.spn_arr, // values/array.xml에 있는 array리소스 인자로 넘기기
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spnSelectField.adapter = adapter // spinner에 array 적용
+
+        }
+
+    }
+}
