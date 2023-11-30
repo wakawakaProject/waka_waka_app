85b67ff6352587fc068a42df9c1e41d4b7e882ea
Hyowon Kang <hyowon3434@naver.com>
2023-11-30 15:18:24 +0900

    Feat: created user DTO

---
+++ app/src/main/java/com/example/waka_waka/User.kt
@@ -0,0 +1,29 @@
+package com.example.waka_waka
+
+class User {
+
+    var id: String = ""
+    var pw: String = ""
+    var name: String = ""
+    var age: String = ""
+    var phone: String = ""
+    var gender: String = ""
+    var position: String = ""
+    var played_year : String = ""
+    var address : String = ""
+
+    constructor()
+
+    constructor(id:String, pw:String, name: String, age: String, phone: String,
+                gender: String, position: String, played_year : String, address : String){
+        this.id = id
+        this.name = name
+        this.pw = pw
+        this. age = age
+        this.phone = phone
+        this.gender = gender
+        this.position = position
+        this.played_year = played_year
+        this. address = address
+    }
+}
