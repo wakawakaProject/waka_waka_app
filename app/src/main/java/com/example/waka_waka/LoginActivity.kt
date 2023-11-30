1a6e0d63fc2ba3e7d0d0e71b2f745156b6d804ca
Hyowon Kang <hyowon3434@naver.com>
2023-11-30 14:43:49 +0900

    Feat: link loginbtn with activity_home

--- app/src/main/java/com/example/waka_waka/LoginActivity.kt
+++ app/src/main/java/com/example/waka_waka/LoginActivity.kt
@@ -7,16 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.waka_waka.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
+
 
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         binding.signUp.setOnClickListener{
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            val signUpintent = Intent(this, SignUpActivity::class.java)
+            startActivity(signUpintent)
         }
+        binding.login.setOnClickListener{
+            val homeIntent = Intent(this,HomeActivity::class.java)
+
+            startActivity(homeIntent)
+        }
+
     }
 }
