2f831691e915de9f5f054da2aa59cf8fa40acc38
Hyowon Kang <hyowon3434@naver.com>
2023-12-02 01:02:48 +0900

    Fix: added social login config

--- app/src/main/java/com/example/waka_waka/LoginActivity.kt
+++ app/src/main/java/com/example/waka_waka/LoginActivity.kt
@@ -2,19 +2,35 @@ package com.example.waka_waka
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.waka_waka.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 
-class LoginActivity : AppCompatActivity() {
+public class LoginActivity : AppCompatActivity() {
 
     val database = Firebase.database
-    val binding = ActivityLoginBinding.inflate(layoutInflater)
+    val ref = database.getReference("user")
     override fun onCreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 구글 소셜로그인 구현부분 (추후 PUSH 예정)
+        /*
+        var setInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId("755533939759-msu2nh9hcla4euaccq95c7e64lkbgkei.apps.googleusercontent.com")
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+*/
+
 
         binding.signUp.setOnClickListener{
             val signUpintent = Intent(this, SignUpActivity::class.java)
@@ -23,6 +39,7 @@ class LoginActivity : AppCompatActivity() {
         binding.login.setOnClickListener{
             val homeIntent = Intent(this,HomeActivity::class.java)
             startActivity(homeIntent)
+
         }
 
     }
