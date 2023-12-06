37e87bf9701c4784e33629fbd7741ce3939b30eb
Hyowon Kang <hyowon3434@naver.com>
2023-12-06 20:35:49 +0900

    Feat: created a new activities and xml

---
+++ app/src/main/java/com/example/waka_waka/MatchListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.waka_waka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_list)
+    }
+}
