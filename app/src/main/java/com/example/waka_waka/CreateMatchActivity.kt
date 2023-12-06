08b9c9c388622b675cc14ecc6d656168c8e72eca
Hyowon Kang <hyowon3434@naver.com>
2023-12-07 00:38:05 +0900

    Feat: 경기장, 경기인원 spinner 구현

--- app/src/main/java/com/example/waka_waka/CreateMatchActivity.kt
+++ app/src/main/java/com/example/waka_waka/CreateMatchActivity.kt
@@ -7,19 +7,31 @@ import com.example.waka_waka.databinding.ActivityMatchCreateBinding
 
 class CreateMatchActivity : AppCompatActivity() {
 
-    val binding = ActivityMatchCreateBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMatchCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // spinner에 리스트 추가
+        // spinner에 경기장 리스트 추가
         ArrayAdapter.createFromResource(
             this,
-            R.array.gender_arr, // values/array.xml에 있는 array리소스 인자로 넘기기
+            R.array.stardium, // values/array.xml에 있는 array리소스 인자로 넘기기
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             binding.spnSelectField.adapter = adapter // spinner에 array 적용
         }
+
+
+        // spinner에 경기 인원수 리스트 추가
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.member_num, // values/array.xml에 있는 array리소스 인자로 넘기기
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spnSelectMember.adapter = adapter // spinner에 array 적용
+        }
+
     }
 }
